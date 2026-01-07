bae9ba344eae3a3f430be09c8580ae25bdccd095
Jeremy O. Rellama <113158566+PaperNsalt@users.noreply.github.com>
2026-01-07 21:03:11 +0800

    Add SadIcon and enhance QuotesPage UI/UX
    Introduced a new SadIcon SVG to IconComponent for error display. Updated QuotesPage with improved layout, added descriptive instructions, enhanced error messaging with SadIcon, and included a 'How It Works' section for better user guidance.

--- src/components/IconComponent.jsx
+++ src/components/IconComponent.jsx
@@ -282,3 +282,23 @@ export const ContactIcon = (
     <path d="M13.832 16.568a1 1 0 0 0 1.213-.303l.355-.465A2 2 0 0 1 17 15h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2A18 18 0 0 1 2 4a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-.8 1.6l-.468.351a1 1 0 0 0-.292 1.233 14 14 0 0 0 6.392 6.384" />
   </svg>
 );
+
+export const SadIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="lucide lucide-frown-icon lucide-frown"
+  >
+    <circle cx="12" cy="12" r="10" />
+    <path d="M16 16s-1.5-2-4-2-4 2-4 2" />
+    <line x1="9" x2="9.01" y1="9" y2="9" />
+    <line x1="15" x2="15.01" y1="9" y2="9" />
+  </svg>
+);
--- src/pages/QuotesPage.jsx
+++ src/pages/QuotesPage.jsx
@@ -4,7 +4,8 @@ import { motion, AnimatePresence } from "motion/react";
 import QuoteCard from "../components/QuoteCardComponent";
 import ButtonLink from "../components/ButtonComponent";
 import QuoteSkeleton from "../components/QuoteSkeletonComponent";
-import { DeleteIcon, SearchIcon } from "../components/IconComponent";
+import ContainerComponent from "../components/ContainerComponent";
+import { DeleteIcon, SadIcon, SearchIcon } from "../components/IconComponent";
 
 const containerVariants = {
   hidden: {},
@@ -110,66 +111,88 @@ function QuotesPage() {
     setLoading(false);
   };
 
+  const spanStyle = "text-[#7fdb0d]";
   return (
     <section className="">
-      <div className="flex flex-col justify-center items-center mx-auto p-6 space-y-6 rounded-3xl mb-10">
-      <h1 className="text-[4rem] font-bold text-center">Quote Generator</h1>
+      <div className="flex flex-col w-[60vw] mt-8 justify-center items-center mx-auto p-6 space-y-6 rounded-3xl mb-10">
+        <h1 className="text-[4rem] font-bold text-center leading-16">
+          Generate
+          <br />
+          Quote
+        </h1>
 
-      {/* CATEGORY */}
-      <div className="flex justify-center">
-        <motion.select
-          whileHover={{
-            boxShadow: "0px 4px 12px rgba(0,0,0,0.08)",
-            scale: 1.05,
-            y: -2,
-          }}
-          whileFocus={{
-            boxShadow: "0px 0px 0px 3px rgba(99,102,241,0.35)",
-            scale: 1.05,
-            y: -2,
-          }}
-          transition={{ duration: 0.2, ease: "easeOut" }}
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-          className="flex justify-evenly px-4 py-2 rounded-xl border"
-        >
-          {CATEGORIES.map((cat) => (
-            <option key={cat} value={cat}>
-              {cat.charAt(0).toUpperCase() + cat.slice(1)}
-            </option>
-          ))}
-        </motion.select>
-      </div>
+        <p className="text-center w-[50vw]">
+          Pick a <span className={spanStyle}>category</span> and instantly
+          generate <span className={spanStyle}> four random quotes </span>{" "}
+          tailored to your mood or interest. Whether you’re seeking motivation,
+          wisdom, or reflection, this app delivers meaningful quotes with a
+          clean interface and smooth interactions.
+        </p>
 
-      {/* BUTTON */}
-      <div className="flex gap-6 justify-center">
-        <ButtonLink
-          icon={
-            loading ? (
-              <motion.div
-                className="w-4 h-4 border-2 border-white border-t-transparent rounded-full"
-                animate={{ rotate: 360 }}
-                transition={{ repeat: Infinity, duration: 0.8, ease: "linear" }}
-              />
-            ) : (
-              SearchIcon
-            )
-          }
-          onClick={fetchQuotes}
-          label={loading ? "Loading" : "Show Quotes"}
-          disabled={loading}
-        />
+        {/* CATEGORY */}
+        <div className="flex justify-center">
+          <motion.select
+            whileHover={{
+              boxShadow: "0px 4px 12px rgba(0,0,0,0.08)",
+              scale: 1.05,
+              y: -2,
+            }}
+            whileFocus={{
+              boxShadow: "0px 0px 0px 3px rgba(99,102,241,0.35)",
+              scale: 1.05,
+              y: -2,
+            }}
+            transition={{ duration: 0.2, ease: "easeOut" }}
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            className="flex justify-evenly flex-col items-center px-2 py-2 rounded-xl border"
+          >
+            {CATEGORIES.map((cat) => (
+              <option key={cat} value={cat}>
+                {cat.charAt(0).toUpperCase() + cat.slice(1)}
+              </option>
+            ))}
+          </motion.select>
+        </div>
 
-        <ButtonLink
-          icon={DeleteIcon}
-          onClick={resetQuotes}
-          label="Reset"
-          disabled={!quotes.length && category === "wisdom"}
-        />
-      </div>
+        {/* BUTTON */}
+        <div className="flex gap-6 justify-center">
+          <ButtonLink
+            icon={
+              loading ? (
+                <motion.div
+                  className="w-4 h-4 border-2 border-white border-t-transparent rounded-full"
+                  animate={{ rotate: 360 }}
+                  transition={{
+                    repeat: Infinity,
+                    duration: 0.8,
+                    ease: "linear",
+                  }}
+                />
+              ) : (
+                SearchIcon
+              )
+            }
+            onClick={fetchQuotes}
+            label={loading ? "Loading" : "Show Quotes"}
+            disabled={loading}
+          />
 
-      {/* ERROR */}
-      {error && <p className="text-center text-red-500">{error}</p>}
+          <ButtonLink
+            icon={DeleteIcon}
+            onClick={resetQuotes}
+            label="Reset"
+            disabled={!quotes.length && category === "wisdom"}
+          />
+        </div>
+
+        {/* ERROR */}
+        {error && (
+          <p className="text-center text-red-500 bg-red-200 py-2 px-4 rounded-2xl flex flex-col justify-center items-center">
+            {SadIcon}
+            {error}
+          </p>
+        )}
       </div>
 
       {/* LOADING */}
@@ -201,13 +224,40 @@ function QuotesPage() {
             className="grid grid-cols-[repeat(auto-fit,minmax(300px,2fr))] gap-6"
           >
             {quotes.map((q, index) => (
-              <motion.div key={index} variants={quoteVariants} layout className="grid grid-cols-[repeat(auto-fit,minmax(300px,2fr))] gap-6">
+              <motion.div
+                key={index}
+                variants={quoteVariants}
+                layout
+                className="grid grid-cols-[repeat(auto-fit,minmax(300px,2fr))] gap-6"
+              >
                 <QuoteCard quote={q.quote} author={q.author} />
               </motion.div>
             ))}
           </motion.div>
         )}
       </AnimatePresence>
+
+      <div className="flex flex-col justify-center items-center mt-70">
+        <h1 className="text-[4rem] px-4 py-2 bg-[#b7ff5e] rounded-3xl">
+          How It Works
+        </h1>
+
+        <div className="grid grid-cols-[repeat(auto-fit,minmax(300px,2fr))] bg-black p-10 mt-10 rounded-3xl">
+          <ContainerComponent text="Users begin by selecting a quote category (such as Motivation, Life, Love, or Wisdom). This selection determines the type of quotes that will be generated." />
+
+          <ContainerComponent
+            text="Once a category is selected, the application sends a request to the backend API. The request includes selected category and required number of quotes (4). This is handled asynchronously to ensure a smooth and responsive user experience."
+          />
+          
+          <ContainerComponent
+            text="The backend handles quote generation by first processing the incoming request and identifying the selected category. It then filters the available quotes to match that category and randomly selects four entries from the filtered dataset. These quotes are formatted into a structured JSON response before being sent back to the frontend."
+          />
+          
+          <ContainerComponent
+            text="Once the quote generation process is complete, the API sends the generated data to the frontend in real time. Each response includes the quote text, the author when available, and the associated category, allowing the frontend to display the information consistently and accurately."
+          />
+        </div>
+      </div>
     </section>
   );
 }
