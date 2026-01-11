5261d54da4920f82cda32909d3c5847561a3a1a6
Jeremy O. Rellama <113158566+PaperNsalt@users.noreply.github.com>
2026-01-11 21:34:07 +0800

    added footer content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,9 @@ function Footer(){
   return (
     <>
     <footer className="mt-10">
-      <div>
-        <h1>Footer</h1>
+      <div className="flex flex-col justify-center items-center ">
+        <h2>Built with React.js and Tailwind CSS.</h2>
+        <p>Copyright © 2025. All rights reserved</p>
       </div>
     </footer>
     </>
